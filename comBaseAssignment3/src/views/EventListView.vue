edaea1c44da157b30fac42e376c4d3aa5ea083ac
Jookmaste <punnlaowatchara@gmail.com>
2026-08-01 22:30:53 +0700

    2.8 CSS Pagination

--- comBaseAssignment3/src/views/EventListView.vue
+++ comBaseAssignment3/src/views/EventListView.vue
@@ -2,10 +2,17 @@
   import EventCard from '@/components/EventCard.vue'
   import EventInfo from '@/components/EventInfo.vue'
   import type { Event } from '@/types'
-  import { ref, onMounted, computed } from 'vue'
+  import { ref, onMounted, computed, watchEffect } from 'vue'
   import EventService from '@/services/EventService'
 
   const events = ref<Event[] | null>(null)
+
+  const totalEvents = ref<number>(0)
+
+  const hasNextPage = computed (() => {
+    const totalPages = Math.ceil(totalEvents.value / 2)
+    return page.value < totalPages 
+  })
 
   const props = defineProps({
     page: {
@@ -18,25 +25,43 @@
 
   onMounted(() => {
 
-    EventService.getEvents(2, page.value)
-      .then((response) => {
-        events.value = response.data
-        console.log(response.data)
-      })
-      .catch((error) => {
-        console.error('There was an error!', error)
-      })
+    watchEffect(() => {
+      events.value = null
+      EventService.getEvents(2, page.value)
+        .then((response) => {
+          events.value = response.data
+          totalEvents.value = response.headers['x-total-count']
+        })
+        .catch ((error) => {
+          console.error('There was an error!', error)
+        })
+    })
   })
 </script>
 
 <template>
 
   <h1>Event For Good</h1>
-  <div class="event">
+  <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <EventInfo v-for="event in events" :key="event.id" :event="event" />
-  </div>
 
+    <div class="pagination">
+      <RouterLink
+      id="page-prev"
+        :to="{ name: 'event-list-view', query: { page: page - 1} }"
+        rel="prev"
+        v-if="page != 1"
+        >&#60; Prev Page</RouterLink>
+
+      <RouterLink
+        id="page-next"
+        :to="{name: 'event-list-view', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+        >Next Page &#62;</RouterLink>
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -44,5 +69,24 @@
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
