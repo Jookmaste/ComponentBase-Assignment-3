76f8687768520613dd67b18b5b25acda80721841
Jookmaste <punnlaowatchara@gmail.com>
2026-08-02 14:02:09 +0700

    5.1 NotFoundView

--- comBaseAssignment3/src/router/index.ts
+++ comBaseAssignment3/src/router/index.ts
@@ -5,6 +5,7 @@ import EventDetailView from '@/views/event/DetailView.vue'
 import EventRegisterView from '@/views/event/RegisterView.vue'
 import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -43,6 +44,11 @@ const router = createRouter({
       name: 'about',
       component: AboutView
     },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
+    }
   ],
 })
 
---
+++ comBaseAssignment3/src/views/NotFoundView.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>Oops!</h1>
+    <h3>The page you're looking for is not here.</h3>
+    <router-like :to="{ name: 'event-list-view' }">Back to the home page</router-like>
+</template>
